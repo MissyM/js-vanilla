eba84758681b22bbc77dd573ac92e19d1f00fa91
Milena <mcabrerapatino@gmail.com>
2020-11-18 16:13:07 -0500

    feat(ui): ad path to readme

---
+++ Constructors/constructors.js
@@ -0,0 +1,21 @@
+function Users(fName, sName ) {
+   this.firstName = fName
+   this.secondName = sName
+}
+
+const user1 = Users("pp","jj")
+const user2 = new Users("dd", "rr") 
+//some api we have to use to from their data and make some  search box like.
+//angularapp-1eb33
+/**
+ * <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.0.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/8.0.2/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
+ */
+npm install -g firebase-tools
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Hello dear developer
 
-If you are here maybe it is because you are trying to learn a bit of pure JavaScript. Thanks for letting me guide you.
+If you are here maybe it is because you are trying to learn a bit of pure JavaScript. Thanks for letting me guide you. You must have node installed.
 To open a file, in the console go to the project file and in the terminal type `node <name_file>.js`
