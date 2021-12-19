4968a8c84497f6390422167773c4e9af03786f1c
MissyM <mcabrerapatino@gmail.com>
2021-12-19 08:26:32 -0500

    FizzBuzz Faster

--- LoopingATriangle/loop2.js
+++ LoopingATriangle/loop2.js
@@ -9,11 +9,18 @@ When you have that working, modify your program to print "FizzBuzz" for numbers 
 (This is actually an interview question that has been claimed to weed out a significant percentage 
   of programmer candidates. So if you solved it, your labor market value just went up.)
 */
-for(i = 1; i <= 100; i++) {
-  if(i % 3 == 0) {
-  	console.log('Fizz');
-  } else if(i % 5 == 0) {
-  	console.log('buzz');
-  }
-  else console.log(i)
+//MissyM
+// for( let i = 1; i < 101; i++) {
+//   if (i % 15 == 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 == 0) {
+//     console.log("Fizz")
+//   } else if (i % 5 == 0) {
+//     console.log("Buzz")
+//   } else {console.log(i)}
+// }
+
+function FizzBuzz(){
+  for (let i = 1; i < 101;i++) console.log((i % 3 ? '' : 'fizz') + (i % 5 ? '' : 'buzz') || i)
 }
+console.log(FizzBuzz())
